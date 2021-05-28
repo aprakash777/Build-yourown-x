bd48426bdc95a593941c921c5c7d23cb86e02dc6
Akshay Prakash <51133712+aprakash7@users.noreply.github.com>
2021-05-27 19:09:36 -0700

    Update LollipopCharts.py

--- Charts and Graphs/LollipopCharts.py
+++ Charts and Graphs/LollipopCharts.py
@@ -9,7 +9,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-table = pd.read_csv(r'C:\Users\Akshay Prakash\Downloads\1617table.csv')
+table = pd.read_csv(r'\1617table.csv')
 table.head()
 plt.hlines(y= np.arange(1, 21), xmin = 0, xmax = table['Pts'], color = 'skyblue')
 plt.plot(table['Pts'], np.arange(1,21), "o")
