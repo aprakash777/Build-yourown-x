b1869064ab0b74ccf11dfae2deae134cf5db6c8b
Akshay Prakash <51133712+aprakash7@users.noreply.github.com>
2021-05-27 19:10:16 -0700

    Scatter Plots

--- Charts and Graphs/ScatterAndCrosshairs.py
+++ Charts and Graphs/ScatterAndCrosshairs.py
@@ -9,7 +9,7 @@ import numpy as np
 import pandas as pd 
 import matplotlib.pyplot as plt 
 
-table = pd.read_csv(r'C:\Users\Akshay Prakash\Downloads\1617table.csv')
+table = pd.read_csv(r'\1617table.csv')
 table.head()
 
 #Creating chart with our own size
